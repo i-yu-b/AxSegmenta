e8f09f252f15acfe0b3ab479721147247543e11b
Barskaya Irina <i.barskaya@partner.samsung.com>
2018-04-10 19:27:43 +0300

    Corrected for python 3.4 error of zip type

--- source-code-clean/data_generator.py
+++ source-code-clean/data_generator.py
@@ -13,12 +13,30 @@ def get_pair(img_path, mask_path):
 def batch_generator(batch_size, train = True):
     if train:
         img_filenames = glob.glob('/notebooks/b.irina/AxSegmenta/Data/Images_patch_224/train/images/control_raw_*')
-        mask_filenames =  glob.glob('/notebooks/b.irina/AxSegmenta/Data/Masks_patch_224/train/images/control_raw_*')
+        mask_filenames = glob.glob('/notebooks/b.irina/AxSegmenta/Data/Masks_patch_224/train/images/control_raw_*')
     else:
         img_filenames = glob.glob('/notebooks/b.irina/AxSegmenta/Data/Images_patch_224/valid/images/control_raw_*')
-        mask_filenames =  glob.glob('/notebooks/b.irina/AxSegmenta/Data/Masks_patch_224/valid/images/control_raw_*')
+        mask_filenames = glob.glob('/notebooks/b.irina/AxSegmenta/Data/Masks_patch_224/valid/images/control_raw_*')
 
-    paired_filenames = zip(img_filenames, mask_filenames)
+    paired_filenames = list(zip(img_filenames, mask_filenames))
+    np.random.shuffle(paired_filenames)
+    img_filenames, mask_filenames = zip(*paired_filenames)
+
+    while True:
+        image_list = []
+        mask_list = []
+        for i in range(batch_size):
+            idx = np.random.randint(0, len(img_filenames))
+            img_path = img_filenames[idx]
+            mask_path = mask_filenames[idx]
+            img, mask = get_pair(img_path, mask_path)
+            image_list.append(img)
+            mask_list.append([mask])
+        image_list = np.array(image_list, dtype=np.float32)
+        mask_list = np.array(mask_list, dtype=np.float32)
+
+        yield image_list, mask_list
+
     np.random.shuffle(paired_filenames)
     img_filenames, mask_filenames = zip(*paired_filenames)
 
