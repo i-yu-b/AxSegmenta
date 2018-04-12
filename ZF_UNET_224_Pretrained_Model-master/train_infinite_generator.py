fc6060e64171449aefa19e203838f90a8e19d8b8
Barskaya Irina <i.barskaya@partner.samsung.com>
2018-04-12 10:45:52 +0300

    Adjusting code for 1 channel input

--- ZF_UNET_224_Pretrained_Model-master/train_infinite_generator.py
+++ ZF_UNET_224_Pretrained_Model-master/train_infinite_generator.py
@@ -17,26 +17,20 @@ from zf_unet_224_model import *
 
 
 def gen_random_image():
-    img = np.zeros((224, 224, 3), dtype=np.uint8)
+    img = np.zeros((224, 224, 1), dtype=np.uint8)
     mask = np.zeros((224, 224), dtype=np.uint8)
 
     # Background
     dark_color0 = random.randint(0, 100)
-    dark_color1 = random.randint(0, 100)
-    dark_color2 = random.randint(0, 100)
     img[:, :, 0] = dark_color0
-    img[:, :, 1] = dark_color1
-    img[:, :, 2] = dark_color2
 
     # Object
     light_color0 = random.randint(dark_color0+1, 255)
-    light_color1 = random.randint(dark_color1+1, 255)
-    light_color2 = random.randint(dark_color2+1, 255)
     center_0 = random.randint(0, 224)
     center_1 = random.randint(0, 224)
     r1 = random.randint(10, 56)
     r2 = random.randint(10, 56)
-    cv2.ellipse(img, (center_0, center_1), (r1, r2), 0, 0, 360, (light_color0, light_color1, light_color2), -1)
+    cv2.ellipse(img, (center_0, center_1), (r1, r2), 0, 0, 360, light_color0, -1)
     cv2.ellipse(mask, (center_0, center_1), (r1, r2), 0, 0, 360, 255, -1)
 
     # White noise
@@ -45,8 +39,6 @@ def gen_random_image():
         for j in range(224):
             if random.random() < density:
                 img[i, j, 0] = random.randint(0, 255)
-                img[i, j, 1] = random.randint(0, 255)
-                img[i, j, 2] = random.randint(0, 255)
 
     return img, mask
 
@@ -61,8 +53,6 @@ def batch_generator(batch_size):
             mask_list.append([mask])
 
         image_list = np.array(image_list, dtype=np.float32)
-        if K.image_dim_ordering() == 'th':
-            image_list = image_list.transpose((0, 3, 1, 2))
         image_list = preprocess_batch(image_list)
         mask_list = np.array(mask_list, dtype=np.float32)
         mask_list /= 255.0
@@ -107,18 +97,11 @@ def train_unet():
 
 
 if __name__ == '__main__':
-    if K.backend() == 'tensorflow':
-        try:
-            from tensorflow import __version__ as __tensorflow_version__
-            print('Tensorflow version: {}'.format(__tensorflow_version__))
-        except:
-            print('Tensorflow is unavailable...')
-    else:
-        try:
-            from theano.version import version as __theano_version__
-            print('Theano version: {}'.format(__theano_version__))
-        except:
-            print('Theano is unavailable...')
+    try:
+        from theano.version import version as __theano_version__
+        print('Theano version: {}'.format(__theano_version__))
+    except:
+        print('Theano is unavailable...')
     print('Keras version {}'.format(__version__))
     print('Dim ordering:', K.image_dim_ordering())
     train_unet()
